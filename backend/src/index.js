9adce83e7ac67ef08f022180c218982c83221a5e
willersilva <willerrodrigo2006@hotmail.com>
2020-04-22 15:35:37 -0300

    Add websocket functionality to when adding new dev appear on mobile in real time

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const routes = require('./routes');
 const cors = require('cors');
+const http = require('http');
+
+const routes = require('./routes');
+const { setupWebsocket } = require('./websocket');
 
 const app = express();
+const server = http.Server(app);
+
+setupWebsocket(server);
 
 mongoose.connect('mongodb+srv://developersmap:developersmap@cluster0-l8gba.mongodb.net/developersmap?retryWrites=true&w=majority',
 {
@@ -16,4 +22,4 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
+server.listen(3333);
